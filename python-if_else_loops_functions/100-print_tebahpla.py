29707d6f9b0ba521b2bcec3cba53f1f7b79289c8
Niz09090 <amanqa6@gmail.com>
2026-04-04 14:14:25 -0400

    test

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for i in range(26):
     if i % 2 == 0:
         print("{}".format(chr(122 - i)), end="")
     else:
-        print("{}".format(chr(89 - i)), end="")
+        print("{}".format(chr(90 - i)), end="")
